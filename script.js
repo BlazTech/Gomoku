e0ba2ee45cc8e52e5d536ff5fa06784d1207287d
Blaz Stanojevic <blaz.stanojevic@skole.hr>
2023-06-28 14:43:55 +0200

    Add table row and column creation

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("test");
+const columns = 21;
+const rows = 21;
+
+const table = document.querySelector("table");
+
+for (let i = 1; i <= rows; i++) {
+    const tr = document.createElement("tr");
+    table.append(tr);
+
+    for (let i = 1; i <= columns; i++) {
+        const th = document.createElement("th");
+        tr.append(th);
+    }
+}
