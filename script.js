43ec6044e8877eed0ecbc18af5f8df0a1203bcc2
Blaz Stanojevic <blaz.stanojevic@skole.hr>
2023-06-28 16:01:38 +0200

    Add table array

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const columns = 21;
 const rows = 21;
+let tableMatrix = [];
 
 const table = document.querySelector("table");
-
-for (let i = 1; i <= rows; i++) {
+for (let i = 0; i < rows; i++) {
+    tableMatrix.push([]);
     const tr = document.createElement("tr");
     table.append(tr);
 
-    for (let i = 1; i <= columns; i++) {
+    for (let j = 0; j < columns; j++) {
+        tableMatrix[i].push("e");
         const th = document.createElement("th");
         tr.append(th);
     }
